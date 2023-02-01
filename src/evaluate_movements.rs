ad54663b9b793e6310d2909727a039c35dd8cef6
Filippo831 <filippoarduini83@gmail.com>
2023-02-01 15:40:10 +0100

    pawn movement

--- src/evaluate_movements.rs
+++ src/evaluate_movements.rs
@@ -1,4 +1,5 @@
 use ggez::mint::Point2;
+use num;
 
 fn evaluate_piece(
     piece: char,
@@ -18,4 +19,27 @@ fn evaluate_pawn(
 ) {
     // if is_white is true (1) * 2 == 2 / 2 == 1 esle if false (0) * 2 == 0 - 1 == -1
     let turn: i32 = is_white as i32 * 2 - 1;
+    let mut row: usize;
+    let mut col: usize;
+    // front movement
+    row = (piece_position.0 + turn) as usize;
+    col = piece_position.1 as usize;
+    if board[row][col] == '0' {
+        possible_movements[row][col] = true;
+    }
+    //left side movement
+    row = (piece_position.0 + turn) as usize;
+    col = (piece_position.1 - 1).abs() as usize;
+    if possible_movements[row][col] {
+        possible_movements[row][col] = true;
+    };
+    // right side
+    row = (piece_position.0 + turn) as usize;
+    col = (piece_position.1 + 1) as usize;
+    if col == 8 {
+        col = 7
+    }
+    if possible_movements[row][col] {
+        possible_movements[row][col] = true;
+    };
 }
