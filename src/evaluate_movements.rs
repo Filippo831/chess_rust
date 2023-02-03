df7ffa0af4daae3ada919edef59893e431aa132d
Filippo831 <filippoarduini83@gmail.com>
2023-02-03 11:47:13 +0100

    started rook evaluation

--- src/evaluate_movements.rs
+++ src/evaluate_movements.rs
@@ -36,10 +36,37 @@ fn evaluate_pawn(
     // right side
     row = (piece_position.0 + turn) as usize;
     col = (piece_position.1 + 1) as usize;
+    // if on the right border don't go further
     if col == 8 {
         col = 7
     }
     if possible_movements[row][col] {
         possible_movements[row][col] = true;
     };
+}
+
+fn evaluate_rook(
+    possible_movements: Vec<Vec<bool>>,
+    piece_position: (i32, i32),
+    board: &Vec<Vec<char>>,
+    is_white: bool,
+) {
+    let turn: i32 = is_white as i32 * 2 - 1;
+    let mut row: usize;
+    let mut col: usize;
+
+    let rook_movements: Vec<i32> = vec![1, 8, -1, -8];
+    let good: Vec<bool> = vec![true, true, true, true];
+
+    // a = multiply factor
+    // m = movement factor
+    for a in 1..7 {
+        for (i, m) in rook_movements.iter().enumerate() {
+            row = (m % 8) as usize;
+            col = (m / 8) as usize;
+            if board[row][col] == '0' {
+                board[row][col] = true;
+            } else if board[row][col].is
+        }
+    }
 }
