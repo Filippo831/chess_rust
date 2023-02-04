40e3ef0d22e7d9174292589d296e3d0290d40e5d
Filippo831 <filippoarduini83@gmail.com>
2023-02-04 12:09:50 +0100

    piece movement evaluation

--- src/evaluate_movements.rs
+++ src/evaluate_movements.rs
@@ -30,7 +30,7 @@ fn evaluate_pawn(
     //left side movement
     row = (piece_position.0 + turn) as usize;
     col = (piece_position.1 - 1).abs() as usize;
-    if possible_movements[row][col] {
+    if board[row][col].is_uppercase() != is_white {
         possible_movements[row][col] = true;
     };
     // right side
@@ -40,33 +40,57 @@ fn evaluate_pawn(
     if col == 8 {
         col = 7
     }
-    if possible_movements[row][col] {
+    if board[row][col].is_uppercase() != is_white {
         possible_movements[row][col] = true;
     };
 }
 
-fn evaluate_rook(
+fn evaluate_strong_piece(
     possible_movements: Vec<Vec<bool>>,
     piece_position: (i32, i32),
     board: &Vec<Vec<char>>,
     is_white: bool,
+    piece_movements: Vec<i32>,
+    number_steps: i32,
 ) {
     let turn: i32 = is_white as i32 * 2 - 1;
-    let mut row: usize;
-    let mut col: usize;
 
-    let rook_movements: Vec<i32> = vec![1, 8, -1, -8];
-    let good: Vec<bool> = vec![true, true, true, true];
+    let good: Vec<bool> = vec![true; piece_movements.len()];
 
     // a = multiply factor
-    // m = movement factor
-    for a in 1..7 {
-        for (i, m) in rook_movements.iter().enumerate() {
-            row = (m % 8) as usize;
-            col = (m / 8) as usize;
-            if board[row][col] == '0' {
-                board[row][col] = true;
-            } else if board[row][col].is
+
+    let initial_position = piece_position.0 * 8 + piece_position.1;
+
+    for a in 0..number_steps {
+        for (i, mov) in piece_movements.iter().enumerate() {
+            if good[i] {
+                let check_position: i32 = initial_position + a * mov;
+                let value: &char = board.iter().flatten().nth(check_position as usize).unwrap();
+
+                // no pieces in that square
+                if *value == '0' {
+                    possible_movements[(check_position / 8) as usize]
+                        [(check_position % 8) as usize] = true;
+                }
+                // opponent piece
+                else if value.is_uppercase() != is_white {
+                    possible_movements[(check_position / 8) as usize]
+                        [(check_position % 8) as usize] = true;
+                    good[i] = false;
+                }
+                // my piece
+                else {
+                    good[i] = false;
+                }
+                // if is checking on the border stop that line
+                if check_position / 8 == 0
+                    || check_position / 8 == 7
+                    || check_position % 8 == 0
+                    || check_position % 8 == 7
+                {
+                    good[i] = false;
+                }
+            }
         }
     }
 }
